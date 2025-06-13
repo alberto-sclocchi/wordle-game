bef6a5f21ed56ea25718d30753d28584d1344aba
Alberto Sclocchi <scloks75tv@gmail.com>
2025-06-13 19:34:03 -0400

    create random five letter words list

--- src/components/wordle/WordleBoard.jsx
+++ src/components/wordle/WordleBoard.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import WordleContext from './context/WordleContext.context'
 import axios from 'axios';
+import { Words } from './model/Words.model';
 
 export default function WordleBoard() {
 
@@ -8,11 +9,17 @@ export default function WordleBoard() {
   const [ guesses, setGuesses ] = useState([]);
 
   useEffect(() => {
+    console.log(Words)
   }, [])
 
   return (
     <div>
         <h1>Wordle Board</h1>
+        {
+            Words.map((word, i) => (
+                <p key={i}>{word}</p>
+            ))
+        }
     </div>
   )
 }
