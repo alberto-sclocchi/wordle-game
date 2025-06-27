5f4414c8b54340f95842c115c1dba9ef8744dba0
Alberto Sclocchi <scloks75tv@gmail.com>
2025-06-26 20:48:15 -0400

    delete start screen page and remove index file routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { WordleProvider } from './components/wordle/context/WordleContext.context';
-import RoutesIndex from './components/core/RoutesIndex';
+import WordleBoard from './components/wordle/WordleBoard';
 
 function App() {
   return (
     <div className="App">
       <WordleProvider>
         <Routes>
-          <Route path="*" element={<RoutesIndex />} />
+          <Route path="/*" element={<WordleBoard />} />
         </Routes>
       </WordleProvider>
     </div>
